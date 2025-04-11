fa36f7c93701148657087070d8e955cd2c657e82
shubhampoxscan <shubham.gupta@poxscan.io>
2025-04-11 15:12:59 +0530

    added miantainence page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-import Login from "./auth/login/page";
+// import Login from "./auth/login/page";
 
 export default function Home() {
   return (
-    <Login/>
+    // <Login/>
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-purple-200 to-purple-600 text-black font-sans text-center">
+    <h1 className="text-4xl font-bold mb-4">We’re Under Maintenance</h1>
+    <p className="text-lg mb-8">
+      Our site is currently undergoing some updates to serve you better.
+      Please check back soon!
+    </p>
+    <div className="w-20 h-20 border-8 border-white border-t-purple-600 rounded-full animate-spin mb-8"></div>
+    <div className="bg-white/10 p-4 rounded-lg text-center">
+      <p className="text-lg italic">
+      &quot;Patience is not the ability to wait, but the ability to keep a good
+      attitude while waiting.&quot;
+      </p>
+      <p className="mt-2 text-sm">— Joyce Meyer</p>
+    </div>
+  </div>
   );
 }
